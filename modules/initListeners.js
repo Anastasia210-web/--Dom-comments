b47cbdc15fd423a1280f6e1c3994ff00df2c3806
Anastasia Arefeva <89157321250@mail.ru>
2025-10-18 16:18:55 +0300

    Module

---
+++ modules/initListeners.js
@@ -0,0 +1,58 @@
+import {comments} from "./comments.js"
+import {sanitizeHtml} from "./sanitizeHtml.js"
+
+export const initLikeListeners = (renderComments) => {
+    const likeButtons = document.querySelectorAll(".like-button");
+    
+            for (const likeButton of likeButtons) {
+              likeButton.addEventListener("click", (event) => {
+                event.stopPropagation();
+    
+                const index = likeButton.dataset.index;
+                const comment = comments[index];
+    
+                comment.likes = comment.isliked
+                ? comment.likes - 1
+                : comment.likes + 1;
+    
+                comment.isliked = !comment.isliked;
+    
+                renderComments();
+              });
+            }
+} 
+
+export const initReplyListeners = () => {
+    const commentsElements = document.querySelectorAll(".comment");
+    const text = document.getElementById("text-input");
+    
+            for (const commentElement of commentsElements) {
+              commentElement.addEventListener("click", () => {
+    
+                const currentComment = comments[commentElement.dataset.index];
+                text.value = `${currentComment.name}: ${currentComment.text}`;
+              });
+            }
+}
+
+export const initAddCommentListener = (renderComments) => {
+    const name = document.getElementById("name-input");
+    const text = document.getElementById("text-input");
+    const addButton = document.querySelector(".add-form-button");
+
+  addButton.addEventListener("click", () => {
+    const newComment = {
+      name: sanitizeHtml(name.value),
+      date: new Date(),
+      text: sanitizeHtml(text.value),
+      likes: 0,
+    };
+
+    comments.push(newComment);
+
+    renderComments();
+
+    name.value = "";
+    text.value = "";
+  });
+}
